6ec5028dc9d4a361bc1451e756e6facc927af547
Kalantar81 <Calantarov@gmail.com>
2021-03-01 13:46:48 +0200

    update

--- src/app/components/product-description/product-description.component.ts
+++ src/app/components/product-description/product-description.component.ts
@@ -11,11 +11,15 @@ import {
 } from '@angular/forms';
 import { IProduct } from 'src/app/services/products.service';
 
+const formatYmd = (date: Date) => date.toISOString().slice(0, 10);
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.less']
 })
+
+
 export class ProductDescriptionComponent  {
 
   public isCreate: boolean;
@@ -32,8 +36,10 @@ export class ProductDescriptionComponent  {
       productName: product.name,
       description: product.description,
       price: product.price,
-      creationDate: product.creationDate,
+      // creationDate: product.creationDate.toDateString(),
+      creationDate: formatYmd(product.creationDate),
     });
+    this.date = product.creationDate;
     console.log(this.form);
   }
 
@@ -41,24 +47,35 @@ export class ProductDescriptionComponent  {
 
   public form = new FormGroup({
     id: new FormControl(),
-    productName: new FormControl('', [Validators.maxLength(30)]),
+    productName: new FormControl(
+      '', 
+      [
+        Validators.required,
+        Validators.maxLength(30)
+      ]
+    ),
     description: new FormControl('', [Validators.required]),
     price: new FormControl('', [Validators.required]),
     creationDate: new FormControl('', [Validators.required]),
   });
 
+  
+  public date: Date;
+
   constructor() { }
 
   public update(): void {
     // Add Validation for form
+    
     if (!this.form.errors) {
       const formData = {...this.form.value};
+      const date = new Date(this.form.value.creationDate.replace( /(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
       this.updateProduct.emit({
         id: undefined,
         name: formData.productName,
         description: formData.description,
         price: formData.price,
-        creationDate: formData.creationDate
+        creationDate: date
       });
     }
   }
